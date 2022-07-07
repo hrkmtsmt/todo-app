f79ce847c184adab6cc960194fab836d68e50512
hrkmtsmt <hrkmtsmt@icloud.com>
2022-07-07 18:46:16 +0900

    :tada: [Initial Commit]: First commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# How to install packages?
+
+```
+# front/
+yarn workspace front add <package-name> <...>
+
+# api/
+yarn workspace api add <package-name> <...>
+```
---
+++ api/src/app.module.ts
@@ -0,0 +1,30 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env', '.env.local'],
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: () => ({
+        type: 'postgres',
+        host: process.env.DB_HOST,
+        port: 5432,
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+        synchronize: true,
+      }),
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
